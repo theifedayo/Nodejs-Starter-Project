238751a03615050146c6368ff9e44a1a871916d9
ifedayo <adesiyanifedayo@gmail.com>
2020-11-20 10:03:51 +0100

    05-Chat-App -Add disconnect from and connect to server

--- 05-Chat-App/app.js
+++ 05-Chat-App/app.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
+const path = require('path')
 
 const app = express()
 const server = http.createServer(app)
 let io = socketIO(server)
 
+const publicPath = path.join(__dirname, 'public')
+app.use(express.static(publicPath))
 
+io.on('connection', (socket)=>{
+	console.log('A new user connected!')
 
-
-
-
-
-
-
+	socket.on('disconnect', ()=>{
+		console.log('User disconnected from server')
+	})
+})
 
 
 
